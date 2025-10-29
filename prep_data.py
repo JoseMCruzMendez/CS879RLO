7c81b38d5579c410b8b387556a630904ac59c31d
Jose Cruz <jmcruz@umass.edu>
2025-10-29 18:00:10 -0400

    Added incorrect labels to dataset

--- prep_data.py
+++ prep_data.py
@@ -2,6 +2,19 @@ import torchvision
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 
+# 1. --- Define the new Dataset class ---
+# This class inherits from MNIST and modifies __getitem__
+# to return (data, label, index)
+class MNISTWithIndices(torchvision.datasets.MNIST):
+    def __getitem__(self, index):
+        # Get the data and target from the parent class (which handles transforms)
+        img, target = super().__getitem__(index)
+
+        # Return the image, target, and the index
+        return img, target, index
+
+# --- Your original code continues from here ---
+
 #I won't normalize for interpretability, but we could try it for performance reasons later
 dataprep = transforms.Compose([
     transforms.ToTensor(),
@@ -10,8 +23,16 @@ dataprep = transforms.Compose([
 
 def prep_data():
     """Downloads MNIST to the data folder if it is not present, and puts it into a DataLoader with batch_size = 128 for easy access. Returns a tuple (train_loader, test_loader)"""
-    train_dataset = torchvision.datasets.MNIST('./data', train=True, download=True, transform=dataprep)
-    test_dataset = torchvision.datasets.MNIST('./data', train=False, download=True, transform=dataprep)
+
+    # 2. --- Use the new class instead of the default MNIST ---
+    train_dataset = MNISTWithIndices(
+        './data', train=True, download=True, transform=dataprep
+    )
+    test_dataset = MNISTWithIndices(
+        './data', train=False, download=True, transform=dataprep
+    )
+
     train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=128, shuffle=False)
+
     return train_loader, test_loader
